07b5209d75af3818f6bf57bc9c8c65714f4028ed
Flavia <flaviamatta90@gmail.com>
2020-10-02 15:46:18 +0200

    start

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//questa è una variabile
+$titolo = "BADWORDS";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>BadWords</title>
+  </head>
+  <body>
+    <h1> <?php echo $titolo; ?></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+  </body>
+</html>
+
+
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
