af916032314cd79ba3e6c4e0a8fe589dee098e90
Flavia <flaviamatta90@gmail.com>
2020-10-02 16:28:35 +0200

    echo strlen

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 $titolo = "BADWORDS";
 $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-var_dump($paragrafo);
+echo strlen($paragrafo);
 
 $nuovoParagrafo = str_replace("sed", "***", $paragrafo);
 ?>
