2fba2f611aa9016285486ddf41408b84b9f1bc8d
Flavia <flaviamatta90@gmail.com>
2020-10-02 17:13:04 +0200

    get 2

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $titolo = "BADWORDS";
-$paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, word sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-$words = "sed";
+$badwords = $_GET["word"];
 
-$_GET["words"];
+// $badwords = "word";
 
 echo strlen($paragrafo);
 
-$nuovoParagrafo = str_replace($words, "***", $paragrafo);
+$nuovoParagrafo = str_replace($badwords, "***", $paragrafo);
 ?>
 
 
